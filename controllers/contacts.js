a767f6e311fff5a31e740673cd0d0352d3688083
Colton Kramer <77127051+coltonkramer@users.noreply.github.com>
2022-09-30 13:21:54 -0400

    Update contacts.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,7 +4,7 @@ const ObjectId = require("mongodb").ObjectId;
 const getData = async (req, res, next) => {
   const result = await mongodb
     .getDb()
-    .db("cs_341_projects")
+    .db("contacts")
     .collection("contacts")
     .find();
   result.toArray().then((lists) => {
@@ -16,7 +16,7 @@ const getDataById = async (req, res, next) => {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb
     .getDb()
-    .db("cs_341_projects")
+    .db("contacts")
     .collection("contacts")
     .find({ _id: userId });
   result.toArray().then((lists) => {
