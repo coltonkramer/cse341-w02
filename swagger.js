7eaaed00b89d5346968d548e3287d9598d4dd24b
coltonkramer <77127051+coltonkramer@users.noreply.github.com>
2022-10-08 15:39:41 -0400

    created a route for api-docs and added swagger functionality

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'Contacts Documentation',
         description: 'Give a clear description of how these routes work and how they can be used'
     },
-    host: 'localhost:8080' | 'https://colton341-w02.onrender.com/',
+    host: 'https://colton341-w02.onrender.com/',
     schemes: ['http'],
 }
 const generatedFile = './swagger.json';
